93a4db7e8debc7348894d9b214dbcc36ec4c0270
Carlos47197 <carlos_alfonso_e@hotmail.com>
2014-06-08 22:20:16 -0500

    Recover

---
+++ EmployeeQuiz/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using EmployeeQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Text;
+using System.Drawing;
+using System.Linq;
+
+namespace EmployeeQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(Employe emp)
+        {
+
+            //Creo el objeto del modelo de datos
+            Payrolldm nomina = new Payrolldm(
+                @"C:\Users\D630\Documents\Visual Studio 2012\Projects\Recover\EmployeeQuiz\Models\EmployeeDb.csv");
+
+            //busco el empleado dado su id
+            var empleado = nomina.GetEmployeById(emp.Id);
+
+
+
+            try
+            {
+                double validacion = double.Parse(emp.Id);
+
+                if (validacion <= 5 && validacion >= 1)
+                {
+
+                    return View("WorkerView", empleado);
+                }
+                else
+                {
+
+                    return View("Error");
+
+                }
+            }
+            catch
+            {
+                return View("Error");
+            }
+               
+            }
+
+
+
+
+    }
+}
